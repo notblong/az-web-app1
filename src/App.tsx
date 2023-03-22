fa3e384c4293dda53314f36285bfcd83433c29ff
Long Pham <longpham.work23@gmail.com>
2023-03-22 21:03:10 +0700

    mm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { User, UsersTable } from './table'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+async function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(new Array<User>());
+
+  axios.get('https://pblo-api-app2.azurewebsites.net/users')
+    .then(response => {
+      console.log(response);
+      setData(response.data)
+    });
+  useEffect(() => {
+  }, []);
 
   return (
     <div className="App">
@@ -28,6 +39,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <UsersTable users={data} />
+
     </div>
   )
 }
