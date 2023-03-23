0622dab0123d65ea571007da1859ec3b696a861d
Pham Bao Long <pblo@netcompany.com>
2023-03-23 18:55:00 +0700

    better with .env variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { UsersTable } from './table'
 
-function App() {1
+function App() {
   const [users, setUsers] = useState([]);
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
@@ -16,29 +16,28 @@ function App() {1
   }, []);
 
   const getUsers = () => {
-    axios.get('https://pblo-api-app1.azurewebsites.net/users')
-      .then(response => {
-        setUsers(response.data)
-      })
+    axios.get(`${import.meta.env.VITE_API_1_ENDPOINT}/users`)
+      .then(response => setUsers(response.data))
       .catch(error => console.log(error));
   }
 
   const add = () => {
-    axios.post('https://pblo-api-app1.azurewebsites.net/users', { name, username, email })
-      .then(response => {
-        setUsers(response.data)
-      })
+    axios.post(`${import.meta.env.VITE_API_1_ENDPOINT}/users`, { name, username, email })
+      .then(response => setUsers(response.data))
       .catch(error => console.log(error));
   }
+
   return (
     <>
       <div className="App">
-        <TextInput placeholder='Name' onChange={(event) => setName(event.target.value)} />
-        <TextInput placeholder='User Name' onChange={(event) => setUsername(event.target.value)} />
-        <TextInput placeholder='Email' onChange={(event) => setEmail(event.target.value)}/>
-        <Button onClick={add}>
-          Submit
-        </Button>
+        <form className='form' onSubmit={(e) => {e.preventDefault(); add()}} >
+          <TextInput placeholder='Name' onChange={(event) => setName(event.target.value)} />
+          <TextInput placeholder='User Name' onChange={(event) => setUsername(event.target.value)} />
+          <TextInput placeholder='Email' onChange={(event) => setEmail(event.target.value)}/>
+          <Button type="submit">
+            Submit
+          </Button>
+        </form>
         <Button onClick={getUsers}>
           Refresh data
         </Button>
