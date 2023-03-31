8cb2f3c397ab24d6116cc858932f2bef53bd4072
Pham Bao Long <pblo@netcompany.com>
2023-03-31 18:29:57 +0700

    added toarst

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
+import { HubConnection } from '@microsoft/signalr/dist/esm/HubConnection';
+import { HubConnectionBuilder } from '@microsoft/signalr/dist/esm/HubConnectionBuilder';
 import { Button, Callout, Card, TextInput } from '@tremor/react';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 import api1 from './apis/api1';
 import './App.css';
 import { UsersTable } from './table';
+import { toast } from 'react-hot-toast';
+import { HttpTransportType } from '@microsoft/signalr';
+import axios from 'axios';
+
 
 function App() {
   const [users, setUsers] = useState([]);
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
-  const emailRegex = new RegExp('/\S+@\S+\.\S+/');
+  const [connection, setConnection] = useState<null | HubConnection>(null);
   
   useEffect(() => {
     getUsers();
+    connectSignalR();
   }, []);
+
+  useEffect(() => {
+    if (connection) {
+      connection
+        .start()
+        .then(() => {
+          connection.on(`generatedDocument`, (message) => {
+            console.log(message);
+            toast(`Document ${message} is created!`);
+          });
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [connection]);
+
+
+  const connectSignalR = async () => {
+    // const connectionInfo = await axios.get('http://localhost:7071/api/negotiate');
+    // console.log('connectionInfo', connectionInfo);
+    
+    const connect = new HubConnectionBuilder()
+      .withUrl("https://pblo-app1-signalr.service.signalr.net/client/?hub=app1signalr", {
+        accessTokenFactory: () => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ii0yMDAyOTEwNjQ4In0.eyJhc3JzLnMuYXV0IjoiV2ViSm9ic0F1dGhMZXZlbCIsIm5iZiI6MTY4MDI2MDYwMCwiZXhwIjoxNjgwMjY0MjAwLCJpYXQiOjE2ODAyNjA2MDAsImF1ZCI6Imh0dHBzOi8vcGJsby1hcHAxLXNpZ25hbHIuc2VydmljZS5zaWduYWxyLm5ldC9jbGllbnQvP2h1Yj1hcHAxc2lnbmFsciJ9.2MvuoUNcr9ie2esphKjKJYvgf654RZ0Mo2jgS6NIZjM',
+        skipNegotiation: true,
+        transport: HttpTransportType.WebSockets,
+        })
+      .withAutomaticReconnect()
+      .build();
+
+    setConnection(connect);
+  }
 
   const getUsers = () => {
     api1.get(`users?subscription-key=${import.meta.env.VITE_API_1_SUBSCRIPTION_KEY}`)
@@ -31,6 +69,8 @@ function App() {
   return (
     <>
       <div className="App">
+        {/* <ToastContainer />  */}
+        <Toaster />
         <Callout
           className="mt-4"
           title="Notifications"
@@ -43,7 +83,8 @@ function App() {
           <form className='form' onSubmit={(e) => { e.preventDefault(); add()}} >
             <TextInput placeholder='Name' onChange={(event) => setName(event.target.value)} />
             <TextInput placeholder='User Name' onChange={(event) => setUsername(event.target.value)} />
-            <TextInput placeholder='Email' onChange={(event) => setEmail(event.target.value)}/>
+            <TextInput placeholder='Email' onChange={(event) => setEmail(event.target.value)} />
+            <br />
             <Button type="submit">
               Submit
             </Button>
--- src/table.tsx
+++ src/table.tsx
@@ -8,8 +8,9 @@ import {
   Text,
   Button
 } from '@tremor/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import api1 from './apis/api1';
+import { toast } from 'react-hot-toast';
 
 export interface User {
   id: string;
@@ -18,27 +19,69 @@ export interface User {
   email: string;
 }
 
+export enum NotificationStatus {
+  notStart = 'notStart',
+  processing = 'processing',
+  done = 'done',
+}
+
 export function UsersTable({ users }: { users: User[] }) {
-  const [loading, setLoading] = useState(false);
+  const [btns, setBtns] = useState({} as any);
+  const notify = (user: User) => toast(() => (<span>Document for <b>{user.username}</b> is generating.Please wait for minutes... </span>), { icon: '💬'});
+
+  const checkDoc = async (uuid: string) => {
+    const resp = await api1.get(`docs/check?subscription-key=${import.meta.env.VITE_API_1_SUBSCRIPTION_KEY}&uuid=${uuid}`);
+    if (resp.data?.status === NotificationStatus.done) {
+      getDoc(uuid);
+      return;
+    }
+
+    if (resp.data?.status === NotificationStatus.notStart) {
+      await generateDoc(uuid);
+      return;
+    }
+  }
 
   const getDoc = (uuid: string) => {
-    setLoading(true);
-    api1.get(`docs?subscription-key=${import.meta.env.VITE_API_1_SUBSCRIPTION_KEY}&uuid=${uuid}`, { responseType: "blob" })
-      .then(response => response.data)
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', `${uuid}.docx`);
-        document.body.appendChild(link);
-        link.click();
+    setTimeout(() => {
+      api1.get(`docs?subscription-key=${import.meta.env.VITE_API_1_SUBSCRIPTION_KEY}&uuid=${uuid}`, { responseType: 'blob' })
+        .then(response => { console.log(response); return response.data; })
+        .then((blob) => {
+          const url = window.URL.createObjectURL(new Blob([blob]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', `${uuid}.docx`);
+          document.body.appendChild(link);
+          link.click();
 
-        // remove
-        link.parentNode?.removeChild(link);
-        URL.revokeObjectURL(url);
-      })
-      .catch(error => console.log(error))
-      .finally(() => setLoading(false));
+          // remove
+          link.parentNode?.removeChild(link);
+          URL.revokeObjectURL(url);
+        })
+        .catch(error => console.log(error))
+        .finally(() => updateStatusBtns([uuid], btns, false));
+    }, 2000);
+  }
+
+  const generateDoc = async (uuid: string) => {
+    console.log("generating...");
+    const resp = await api1.get(`docs/generate?subscription-key=${import.meta.env.VITE_API_1_SUBSCRIPTION_KEY}&uuid=${uuid}`);
+    const user = users.find(u => u.id === uuid);
+    notify(user as User);
+    updateStatusBtns([uuid], btns, false);
+    return resp.data;
+  }
+
+  useEffect(() => {
+    const obj = {} as any;
+    users.forEach(user => obj[user.id] = false);
+    setBtns(obj);
+  }, [users]);
+
+  const updateStatusBtns = (uuids: string[], btns: any, status: boolean) => {
+    const obj = Object.assign({}, btns);
+    uuids.forEach(uuid => obj[uuid] = status);
+    setBtns(obj);
   }
 
   return (
@@ -63,7 +106,7 @@ export function UsersTable({ users }: { users: User[] }) {
                 <Text>{user.email}</Text>
               </TableCell>
               <TableCell>
-                <Button onClick={() => getDoc(user.id)}>Download</Button>
+                <Button onClick={() => { updateStatusBtns([user.id], btns, true); checkDoc(user.id) }} loading={btns[user.id]}>Download</Button>
               </TableCell>
             </TableRow>
           ))}
